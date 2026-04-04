06539e9226957bd626ddb685183660927c19ae28
kpal2 <amiteshanand57484@gmail.com>
2026-04-04 12:05:20 +0530

    Fix quick link

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,22 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({ top: 0, left: 0, behavior: "auto" });
+  }, [pathname]);
+
+  return null;
+};
 
 const Layout = ({ children }) => {
   return (
     <div className="appShell">
+      <ScrollToTop />
       <Navbar />
       <main className="pageWrap">{children}</main>
       <Footer />
